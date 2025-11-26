5889ab5ea36e291907e04d74b862483f704133a0
reproducepark <reproducepark@kaist.ac.kr>
2025-11-26 16:29:55 +0900

    task 1-0

--- 2d_plot_fm_todo/network.py
+++ 2d_plot_fm_todo/network.py
@@ -1,3 +1,4 @@
+# Noise Prediction Network의 코드
 import math
 from typing import List
 
@@ -5,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
+# TimeEmbedding Layer : 시간을 벡터 형태로 표현
 class TimeEmbedding(nn.Module):
     def __init__(self, hidden_size, frequency_embedding_size=256):
         super().__init__()
@@ -28,11 +29,14 @@ class TimeEmbedding(nn.Module):
         """
         # https://github.com/openai/glide-text2im/blob/main/glide_text2im/nn.py
         half = dim // 2
+        # 여러 주파수를 생성
+        # logarithmic frequency spacing : 로그 스케일로 주파수를 생성
         freqs = torch.exp(
             -math.log(max_period)
             * torch.arange(start=0, end=half, dtype=torch.float32)
             / half
         ).to(device=t.device)
+        # 각 주파수에 대해 사인과 코사인을 계산
         args = t[:, None].float() * freqs[None]
         embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
         if dim % 2:
@@ -41,6 +45,8 @@ class TimeEmbedding(nn.Module):
             )
         return embedding
 
+    # MLP를 사용하여 task-adaptive 하게 표현
+    # 모델이 필요한 특성을 활용하도록 학습
     def forward(self, t: torch.Tensor):
         if t.ndim == 0:
             t = t.unsqueeze(-1)
@@ -48,7 +54,7 @@ class TimeEmbedding(nn.Module):
         t_emb = self.mlp(t_freq)
         return t_emb
 
-
+# TimeLinear Layer : 시간을 함께 입력받는 linear layer
 class TimeLinear(nn.Module):
     def __init__(self, dim_in: int, dim_out: int, num_timesteps: int):
         super().__init__()
@@ -59,10 +65,12 @@ class TimeLinear(nn.Module):
         self.time_embedding = TimeEmbedding(dim_out)
         self.fc = nn.Linear(dim_in, dim_out)
 
+    # 시간 임베딩으로 만든 스케일링 벡터와 output을 element-wise product
     def forward(self, x: torch.Tensor, t: torch.Tensor):
         x = self.fc(x)
         alpha = self.time_embedding(t).view(-1, self.dim_out)
 
+        #element-wise product
         return alpha * x
 
 
@@ -82,8 +90,12 @@ class SimpleNet(nn.Module):
         """
 
         ######## TODO ########
-        # DO NOT change the code outside this part.
-
+        # 입력 차원부터 출력 차원까지 담은 리스트
+        dims = [dim_in] + list(dim_hids) + [dim_out]
+        # TimeLinear 레이어 리스트 생성
+        self.layers = nn.ModuleList(
+            [TimeLinear(dims[i], dims[i+1], num_timesteps) for i in range(len(dims)-1)]
+        )
         ######################
         
     def forward(self, x: torch.Tensor, t: torch.Tensor):
@@ -96,7 +108,10 @@ class SimpleNet(nn.Module):
             t: the time that the forward diffusion has been running
         """
         ######## TODO ########
-        # DO NOT change the code outside this part.
-
+        # (Noise를 예측하므로) last layer를 제외하고 timelinear layer에 활성화 함수 적용
+        for i, layer in enumerate(self.layers):
+            x = layer(x, t)
+            if i < len(self.layers) - 1:
+                x = F.relu(x)
         ######################
         return x
